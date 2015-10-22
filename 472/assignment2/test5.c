31b7e55fe51e45e89d1298a0c4c42ed42165a24c
neale <nealeratzlaff@gmail.com>
2015-10-22 15:11:03 -0700

    try 472 assignment2 on flip

--- 472/assignment2/test5.c
+++ 472/assignment2/test5.c
@@ -1,55 +1,69 @@
 #include <stdio.h>
 #include <stdint.h>
-
-typedef union {
-  float f;
-  struct {
-    unsigned int mantisa : 23;
-    unsigned int exponent : 8;
-    unsigned int sign : 1;
-  } parts;
-} double_cast;
+#include <math.h>
 
 typedef union {
     double d;
     uint64_t dcast;
 } char_cast;
 
+//int constexpr _pow(int base, int exp) {
+//    return exp == 0 ? 1 : base * _pow(base, exp - 1);
+//}
+
 double frexp_test(double x, int *exponent) {
-   
-    int i = 0;
-    int bias = 1023; 
+    
     char_cast test;
     test.d = x;
-    printf("test uint64: %llu\n", test.dcast);
+
+    int i = 0;
+    uint64_t j = 1;
+    uint64_t bit = 0;
+    uint64_t power = 0;
+    double fraction = 0;
+    double frac = 0;
+    double normal_mantissa = 0;
     uint64_t mantissa = test.dcast & 0xFFFFFFFFFFFFF;
     uint64_t exp = (test.dcast >> 52) & 0x7FF;
     unsigned int sign = test.dcast >> 63;
+
     printf("sign = %d\n",sign);
     printf("exponent = %llu\n",exp-1023);
     printf("mantissa = %llu\n",mantissa);
-    for (i = 53; i > 0; i--) {
-        //start at end, for each bit multiply by 2^-i
-        (mantissa >> i)
+
+    for (i = 51; i >= 0; i--) {
+        bit = (mantissa >> i) & 0x01;
+        if (bit) {
+            power = (1 << j);
+            fraction = (double) power;
+            frac = (double) 1./fraction;
+            normal_mantissa += bit * frac;
+        }
+        
+        printf("i               : %d\n", i);
+        printf("j               : %llu\n", j);
+        printf("bit             : %llu\n", bit);
+        printf("power           : %llu\n", power);
+        printf("fraction        : %lf\n", fraction);
+        printf("frac            : %lf\n", frac);
+        printf("normal_mantissa : %lf\n\n", normal_mantissa);
+        j++;
+    }
+    printf("normalized mantissa : %lf\n", 1+normal_mantissa);
     return 1;
 }
 
 int main() {
-  double_cast d1;
-  d1.f = 0.15625;
-  printf("sign = %u\n",d1.parts.sign);
-  printf("exponent = %u\n",d1.parts.exponent);
-  printf("mantisa = %u\n",d1.parts.mantisa);
-
+  
   double this = 3232.4;
   int shit;
   frexp_test(this, &shit);
-  return 1; 
   char_cast d2;
-  d2.d = 0.15625;
-  printf("chars = %s\n",d2.bin);
-  unsigned int fart = (d2.dcast >> 53) & 0x7FF;
-  printf("fart : %u\n", fart);
+  double man, fuck;
+  int another;
+  fuck = 4.5;
+  man  = frexp(fuck, &another);
+    printf("%lf = %lf * 2 ^ %d\n", fuck, man, another);
   return 0;
 
 }
