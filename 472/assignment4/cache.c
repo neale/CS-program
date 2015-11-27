da79972ea02ac6bd6a9be6558a1d3af3f0d19f76
neale <nealeratzlaff@gmail.com>
2015-11-27 13:20:00 -0800

    try it

--- 472/assignment4/cache.c
+++ 472/assignment4/cache.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
@@ -6,7 +7,7 @@
 #include <time.h>
 #include <limits.h>
 #include <memory.h>
-
+#define SIZE_ARR 2000000000
 
 uint64_t rdtsc(void){
     unsigned a, d;
@@ -18,31 +19,35 @@ int check_cache(){
 
     int i = 0;
     int j = 0;  
-    int size_of_array = 1024 * 1024;
-    unsigned char buffer[size_of_array];
-    int cycles = sizeof(buffer)/sizeof(buffer[0]);
+
+    double stride = 1024;
+    int stride_inc = 4096;
+    unsigned char *huge = malloc(SIZE_ARR);
     long long int a, b, c;
-    FILE *fp;
-    fp = fopen("output.txt", "w");
-    if (fp == NULL){
-        printf("File does not exists \n");
-        return 1;
-    }
+    
     float size = 0;
     double time_seconds;
     int freq = 3166000; //using this will give you time in ms
-    for(i = 0; i < cycles; i += 1024) {
+    double old_time = 0;
+    double factor = 0;
+    int count = 0;
+    while (stride < SIZE_ARR) {
         a = rdtsc();
-        for(j = 0; j < i; j++) {
-            buffer[j]++;
+        count = 0;
+        for(i = 0; i < stride; i+=128) {
+            huge[i]++;
+            count++;
         }
         b = rdtsc();
         c = b - a;
+        old_time = time_seconds;
         time_seconds = (double) c / freq;
-        size = (float) i / 1024;
-        fprintf(fp, "Size: %0.3fKB, Time: %lf ms\n", size, time_seconds);
+        size = (float) stride/1024;
+        factor = time_seconds/old_time;
+        //printf("stride size: %d, time: %lf, factor_inc %0.3f\n", stride, time_seconds, factor);
+        fprintf(stdout, "Size: %0.3fKB,\tTime: %lf ms,\tpercent: %lf\n", size, time_seconds, stride/SIZE_ARR);
+        stride *= 2;
     }
-    fclose(fp);
     return 0;
 }
 
