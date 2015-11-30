0d99a08445538520845e378373efb05d97c31ed5
Neale Ratzlaff <ratzlafn@flip2.engr.oregonstate.edu>
2015-11-30 15:51:52 -0800

    -m

--- 344/assignment5/mypython.py
+++ 344/assignment5/mypython.py
@@ -2,7 +2,7 @@
 for i in range(3):
     with open('./txt-file-'+str(i)+'.txt', 'wb+') as f:
         for x in {f.write, __import__('__builtin__').__dict__['print']}:
-            x(str(''.join(([__import__('random').__dict__['choice'](map(chr,range(97, 123))) for n in range(10)]))))
+            x(str(''.join(([__import__('random').__dict__['choice'](list(map(chr,range(97, 123)))) for n in range(10)]))))
 a , b = __import__('random').__dict__['randint'](1, 43),__import__('random').__dict__['randint'](1, 43)
-print "{}, {}\n{}".format(a, b, a*b)
+print ("{}, {}\n{}").format(a, b, a*b)
 #really couldn't figure a way to do line 6 and 7 in one line. Python doesn't actuall eval in a print statement. 
