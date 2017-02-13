e8d8eeebdbf677700fc45f07ac363f62554936f3
neale <nealeratzlaff@gmai.com>
2017-02-12 18:15:59 -0800

    added more grads

--- 519-DeepLearning/assignment1/MLP_Skeleton.py
+++ 519-DeepLearning/assignment1/MLP_Skeleton.py
@@ -11,8 +11,6 @@ import sys
 import cPickle
 import numpy as np
 
-# This is a class for a LinearTransform layer which takes an input 
-# weight matrix W and computes W x as the forward step
 """ This will be the interior of the sigmoid cross entropy function, 
 this is what also will be conputed at each layer. We can vectorize this 
 simply by using numpy matrix ops. The forward operation is simply a dot(W, x)
@@ -21,20 +19,15 @@ and the backward pass will accumulate gradients and update the weights"""
 class LinearTransform(object):
 
     def __init__(self, W):
-	# DEFINE __init function
         self.W = W
 
     def forward(self, x):
-	# DEFINE forward function
         return np.dot(W.T, x)
 
-    def backward(self, grad_output, lr=0.0, momentum=.9, l2=0.0):
-	# DEFINE backward function
+    def backward(self, grad_output, lr, momentum, l2):
         weight_decay = l2 * np.linalg.norm(self.W)
         self.W = self.W - lr * momentum * grad_output * weight_decay
-    # ADD other operations in LinearTransform if needed
 
-# This is a class for a ReLU layer max(x,0)
 """ We just want to take the layer wise maximum of the inputs 
 for the forward pass, and we want to truncate only the gradients 
 in the backward pass"""
@@ -60,7 +53,6 @@ class SigmoidCrossEntropy(object):
 
 	def backward(self, x, y):
 	    # DEFINE backward function
-            self.W = self.W - lr * momentum * grad_output * weight_decay
             return self.forward(x)* (1. - self.forward(x))
            
 # This is a class for the Multilayer perceptron
@@ -69,24 +61,32 @@ class MLP(object):
     def __init__(self, input_dims, hidden_units):
         # INSERT CODE for initializing the network
         
-        self.initialW = np.random.randn(input_dims + hidden_units + 1, 4 * hidden_units) / np.sqrt(input_dims + hidden_units)
-        self.initialW[0,:] = 1 #init biases to 1
-        self.grad = None
-        self.dInput = input_dims
-        self.loss = SigmoidCrossEntropy()
-        self.transform = LinearTransform(self.initialW)
-        self.relu = ReLU()
+        self.initialWI       = np.random.randn(input_dims + hidden_units + 1, 4 * hidden_units) / np.sqrt(input_dims + hidden_units)
+        self.initialWH       = np.random.randn(input_dims + hidden_units + 1, 4 * hidden_units) / np.sqrt(input_dims + hidden_units)
+        self.initialWI[0,:]  = 1 #init biases to 1
+        self.initialWH[0,:]  = 1 #init biases to 1
+        self.grad            = None
+        self.forward_result  = None
+        self.dInput          = input_dims
+        self.k               = hidden_units
+        self.loss            = SigmoidCrossEntropy()
+        self.InputTransform  = LinearTransform(self.initialWI)
+        self.HiddenTransform = LinearTransform(self.initialWH)
+        self.relu            = ReLU()
 
     # one train is a forward and backward method
     def train(self, x, y, lr, momentum, l2,):
 	# one forward pass
-        x = self.transform.forward(x)
-        x = self.relu.forward(x)
-        res = self.loss.forward(x, transform)
+        x   = self.InputTransform.forward(x)
+        x   = self.relu.forward(x)
+        x   = self.HiddenTransform.forward(x)
+        self.forward_result = self.loss.forward(x, transform)
+
         # now the backward
         self.grad = self.loss.backward(x, y)
+        self.grad = self.HiddenTransform(x, self.grad)
         self.grad = self.relu.backward(x, self.grad)
-        self.transform.backward(x, self.grad)
+        self.grad = self.InputTransform(x, self.grad)
           
     # one eval is one forward
     def evaluate(self, x, y):
@@ -112,8 +112,8 @@ if __name__ == '__main__':
     hidden_units = 10
     num_batches = 1000
     num_examples, input_dims = train_x.shape
-
-    mlp = MLP(input_dims, hidden_units)
+    args = [lr, momentum, l2, input_dims, hidden_units]
+    mlp = MLP(input_dims, hidden_units, args)
     for epoch in xrange(num_epochs):
         for b in xrange(num_batches):
 
